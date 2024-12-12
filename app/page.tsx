0706f5f99e29c540290d1a74aa0375d284afe9b0
Jonathan Meij <jonathanvanmeij@gmail.com>
2024-12-12 20:22:35 +0100

    homepage changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Navbar from "./components/navbar";
 import { SanityDocument } from "next-sanity";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import imageUrlBuilder from "@sanity/image-url";
+import Image from "next/image";
 
 const options = { next: { revalidate: 120 } };
 
@@ -15,72 +16,87 @@ const POSTS_QUERY = `*[
   ]`;
 
 export default async function IndexPage() {
-    const products = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
-
     return (
-        <div>
-            <main className="paper-bg  h-full header-mask">
-                <div className="relative">
-                    <Navbar className="absolute top-0 z-50" />
-                    <div
-                        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 space-y-2 -translate-y-1/2
+        <>
+            <div className="paper-bg header-mask">
+                <Header />
+                <WelcomeSection />
+            </div>
+            <ProductSection />
+        </>
+    );
+}
+
+function Header() {
+    return (
+        <div className="relative">
+            <Navbar className="absolute top-0 z-50" />
+            <div
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 space-y-2 -translate-y-1/2
  z-10"
-                    >
-                        <h1 className="text-4xl font-bold text-white max-w-md text-center ">
-                            Huisportretten en uitgeprinte tekeningen
-                        </h1>
-                        <p className="text-white text-center text-lg">
-                            Waterverf en digitale illustraties door Jaap
-                        </p>
-                    </div>
-                    <div className="header-mask overflow-hidden">
-                        <img
-                            alt="Watercolor painting of a bridge"
-                            src={"/images/headerPicture.png"}
-                            className="object-cover w-full h-[30rem] scale-150 sm:scale-100"
-                        />
-                        <div className="absolute top-0 bg-gradient-to-b to-black/50  via-30% via-black/50 from-black/80 h-full w-full"></div>
-                    </div>
-                </div>
-                <Container className="pb-28 pt-8 sm:pt-10" size={"sm"}>
-                    <h1 className="text-4xl font-bold mb-8">Over mij</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                        mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-                        voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-                        fugiat iusto fuga praesentium optio, eaque rerum! Provident
-                        similique accusantium nemo autem. Veritatis obcaecati tenetur iure
-                        eius earum ut molestias architecto voluptate aliquam nihil,
-                        eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-                        tenetur error, harum nesciunt ipsum debitis quas aliquid.
-                        Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa
-                        laudantium molestias eos sapiente officiis modi at sunt excepturi
-                        expedita sint? Sed quibusdam recusandae alias error harum maxime
-                        adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
-                        Officiis iure rerum voluptates a cumque velit quibusdam sed amet
-                        tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat,
-                        temporibus enim commodi iusto libero magni deleniti quod quam
-                        consequuntur! Commodi minima excepturi repudiandae velit hic
-                        maxime doloremque. Quaerat provident commodi consectetur veniam
-                        similique ad earum omnis ipsum saepe, voluptas, hic voluptates
-                        pariatur est explicabo fugiat, dolorum eligendi quam cupiditate
-                        excepturi mollitia maiores labore suscipit quas? Nulla, placeat.
-                        Voluptatem quaerat non architecto ab laudantium modi minima sunt
-                        esse temporibus sint culpa, recusandae aliquam numquam totam
-                        ratione voluptas quod exercitationem fuga. Possimus quis earum
-                        veniam quasi aliquam eligendi, placeat qui corporis!
-                    </p>
-                </Container>
-            </main>
-            <Container size="sm" className="-mt-12 sm:mt-0">
-                <h1 className="text-4xl   font-bold mb-8">Producten</h1>
-                <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
-                    {products.map((product) => (
-                        <ProductCard key={product.title} document={product} />
-                    ))}
-                </div>
-            </Container>
+            >
+                <h1 className="text-4xl font-bold text-white max-w-md text-center ">
+                    Huisportretten en uitgeprinte tekeningen
+                </h1>
+                <p className="text-white text-center text-lg">
+                    Waterverf en digitale illustraties door Jaap
+                </p>
+            </div>
+            <div className="header-mask overflow-hidden">
+                <img
+                    alt="Watercolor painting of a bridge"
+                    src={"/images/headerPicture.png"}
+                    className="object-cover w-full h-[30rem] scale-150 sm:scale-100"
+                />
+                <div className="absolute top-0 bg-gradient-to-b to-black/50  via-30% via-black/50 from-black/80 h-full w-full"></div>
+            </div>
         </div>
+    );
+}
+
+function WelcomeSection() {
+    return (
+        <Container className=" py-20 flex gap-8 items-center" size={"sm"}>
+            <div>
+                <h1 className="text-4xl font-bold mb-8">Hoi ik ben Jaap</h1>
+                <p>
+                    Ben je op zoek naar een uniek en persoonlijk cadeau dat écht indruk
+                    maakt? Dan ben je hier aan het juiste adres! Ik creëer op maat
+                    gemaakte tekeningen, zoals prachtige huisportretten of gedetailleerde
+                    illustraties van winkels en bedrijfspanden. Heb je iets anders in
+                    gedachten? Geen probleem! Denk bijvoorbeeld aan een tekening van je
+                    boot, een doop- of belijdenistekst gecombineerd met een illustratie
+                    van je kerk, of een ander bijzonder idee. Wat jouw wens ook is, ik
+                    zorg ervoor dat het speciaal en helemaal op maat wordt gemaakt.
+                    Daarnaast maak ik ook muurtekeningen! Kies een bestaand ontwerp of
+                    laat er een speciaal voor jou ontwerpen. Samen zorgen we voor een
+                    unieke en persoonlijke creatie die helemaal bij jou past. Laat je
+                    inspireren en neem gerust contact op samen maken we jouw cadeau of
+                    kunstwerk onvergetelijk!
+                </p>
+            </div>
+            <Image
+                src={"/images/profiel.png"}
+                alt="Jaap"
+                width={300}
+                height={300}
+                className="rounded-2xl shadow-lg"
+            />
+        </Container>
+    );
+}
+
+async function ProductSection() {
+    const products = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
+    return (
+        <Container size="sm" className=" sm:mt-0">
+            <h1 className="text-4xl   font-bold mb-8">Producten</h1>
+            <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
+                {products.map((product) => (
+                    <ProductCard key={product.title} document={product} />
+                ))}
+            </div>
+        </Container>
     );
 }
 
@@ -106,7 +122,7 @@ function ProductCard({ document }: { document: SanityDocument }) {
             <h1 className="text-xl font-bold mt-2 group-hover:underline">
                 {document.title}
             </h1>
-            <p>{document.description[0].children[0].text}</p>
+            {/* <p>{document.description[0].children[0].text}</p> */}
         </a>
     );
 }
