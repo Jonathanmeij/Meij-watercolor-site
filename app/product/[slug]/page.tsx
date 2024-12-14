4a1bd40a9ead16ea0f7d17265243f6a191c2456a
Jonathan Meij <jonathanvanmeij@gmail.com>
2024-12-14 15:43:43 +0100

    yes

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -30,19 +30,24 @@ export default async function PostPage({
     const postImageUrl = post.image
         ? urlFor(post.image)?.width(550).height(550).url()
         : null;
+    const images: string[] = post.paginaFotos
+        ? post.paginaFotos.map((image: SanityImageSource) => {
+              return urlFor(image)?.width(550).height(550).url();
+          })
+        : [];
 
     return (
         <div className=" min-h-screen">
             <main>
                 <Navbar className="!text-green-950 " />
-                <Container size={"sm"} className="pt-0">
+                <Container size={"sm"} className="pt-0 space-y-6">
                     <div className="py-6">
                         <Link href="/" className="hover:underline">
                             ← Terug naar overzicht
                         </Link>
                     </div>
 
-                    <div className="flex gap-6 items-center md:flex-row flex-col">
+                    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 items-center">
                         <div className="w-full">
                             <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
                             <div className="prose">
@@ -61,9 +66,48 @@ export default async function PostPage({
                             />
                         )}
                     </div>
+                    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+                        {images[0] && (
+                            <Image
+                                src={images[0]}
+                                alt={post.title}
+                                className="rounded-xl "
+                                width="500"
+                                height="500"
+                            />
+                        )}
+                        {images[1] && (
+                            <Image
+                                src={images[1]}
+                                alt={post.title}
+                                className="rounded-xl "
+                                width="500"
+                                height="500"
+                            />
+                        )}
+                    </div>
                 </Container>
-                <ProcesSection title="Huisportret" />
+                <ProcesSection title={post.title} />
                 <Contact />
+                {images.length > 2 && (
+                    <Container size={"sm"}>
+                        <h2 className="text-3xl font-bold mb-8">Meer {post.title}</h2>
+                        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                            {images
+                                .filter((_, i) => i > 1)
+                                .map((image, index) => (
+                                    <Image
+                                        key={index}
+                                        src={image}
+                                        alt={post.title}
+                                        className="rounded-xl"
+                                        width="500"
+                                        height="500"
+                                    />
+                                ))}
+                        </div>
+                    </Container>
+                )}
             </main>
         </div>
     );
