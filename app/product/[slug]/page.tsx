03dc6549c00309de8d2cdeb4feb16e0293da648b
Jonathan Meij <jonathanvanmeij@gmail.com>
2024-12-14 16:44:23 +0100

    spacing

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -76,7 +76,7 @@ export default async function PostPage({
                             />
                         )}
 
-                        <div className="space-y-6">
+                        <div className="space-y-4">
                             <h2 className="text-3xl  font-bold">
                                 {post.title} laten maken?
                             </h2>
